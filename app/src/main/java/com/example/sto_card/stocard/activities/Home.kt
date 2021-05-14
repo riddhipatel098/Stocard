06787ffb4008ea235e816cf29faff3991f6e69ac
Ridhi <riddhipatel780677@gmail.com>
2021-05-14 11:10:52 +0530

    Invite USEr completed

--- app/src/main/java/com/example/sto_card/stocard/activities/Home.kt
+++ app/src/main/java/com/example/sto_card/stocard/activities/Home.kt
@@ -2,23 +2,23 @@ package com.example.sto_card.stocard.activities
 
 import android.content.Intent
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.Choreographer
+import android.transition.Explode
+import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.sto_card.BuildConfig
 import com.example.sto_card.R
 import com.example.sto_card.stocard.fragements.*
 import com.example.sto_card.stocard.modals.SharedPrefManager
 import com.google.android.material.navigation.NavigationView
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
 
 
 class Home : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
@@ -26,6 +26,7 @@ class Home : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListen
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var toolbar:Toolbar
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -88,6 +89,20 @@ class Home : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListen
                 startActivity(Intent(this, IntroScreen::class.java))
                 true
             }
+            R.id.Have_A_code->
+            {
+                openFragment(EnterRefferalCodeFragment())
+            }
+            R.id.Invite_User->
+            {
+                val shareIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.example.stocardapp")
+                    type = "text/plain"
+                }
+                startActivity(shareIntent)
+               // openFragment(EnterRefferalCodeFragment())
+            }
         }
         drawer.closeDrawer(GravityCompat.START)
         return true
@@ -104,6 +119,11 @@ class Home : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListen
         transaction.replace(R.id.fragment_container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
